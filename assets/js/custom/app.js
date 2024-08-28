8ba7d34168d20c10a4a5abae53e98f93800629fa
shakibul <mdshakibulalam@gmail.com>
2024-08-28 08:03:40 +0600

    profile picture added

--- assets/js/custom/app.js
+++ assets/js/custom/app.js
@@ -1,29 +1,33 @@
-
 // adding check icon for project list
 const addCheckIconInProjectList = () => {
-    const projects = document.getElementsByClassName('project-check');
-    const projectsCopy = Array.from(projects);
+  const projects = document.getElementsByClassName("project-check");
+  const projectsCopy = Array.from(projects);
 
-    projectsCopy.forEach(function (item) {
-        const projectCheck = createProjectCheckIcon();
-        item.replaceWith(projectCheck);
-    });
-}
+  projectsCopy.forEach(function (item) {
+    const projectCheck = createProjectCheckIcon();
+    item.replaceWith(projectCheck);
+  });
+};
 
 function createProjectCheckIcon() {
-    const projectCheck = document.createElement('img');
-    projectCheck.src = "./assets/svg/check-solid.svg";
-    projectCheck.width = "20";
-    projectCheck.height = "20";
-    projectCheck.title = "Checkmark";
-    projectCheck.alt = "Checkmark";
+  const projectCheck = document.createElement("img");
+  projectCheck.src = "./assets/svg/check-solid.svg";
+  projectCheck.width = "20";
+  projectCheck.height = "20";
+  projectCheck.title = "Checkmark";
+  projectCheck.alt = "Checkmark";
 
-    return projectCheck;
+  return projectCheck;
 }
 
 function setExperienceYears(experienceYears) {
-    const experience = document.getElementsByClassName('experienceYears');
-    for (let i = 0; i < experience.length; i++) {
-        experience[i].innerHTML = experienceYears;
-    }
+  const experience = document.getElementsByClassName("experienceYears");
+  for (let i = 0; i < experience.length; i++) {
+    experience[i].innerHTML = experienceYears;
+  }
+}
+
+function customizePicture(selection, size) {
+  selection.setAttribute("width", size);
+  selection.setAttribute("height", size);
 }
