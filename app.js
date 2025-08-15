0ebb66baa3e244bce095967f7e9e7fa8f8e09da5
shakibul <mdshakibulalam@gmail.com>
2025-08-15 15:24:41 +0600

    refactoring and comment out the tech part in projects

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ import setExperienceYears from './assets/js/custom/experiance.js';
 import {addCheckIconInProjectList} from "./assets/js/custom/check-icon.js";
 import customizePicture from "./assets/js/custom/customize-profile-pic.js";
 import {loadComponents} from "./components.js";
+import {expYears} from "./constants.js";
 
 const componentsLoaded = await loadComponents();
 
 if (componentsLoaded) { // Check if components loaded successfully
-    setExperienceYears("03+");
+    setExperienceYears(expYears);
     addCheckIconInProjectList();
     customizePicture(document.getElementById("profile-pic"), 200);
     // ... any other code that depends on the components being loaded ...
---
+++ constants.js
@@ -0,0 +1,1 @@
+export const expYears = "3.5";
